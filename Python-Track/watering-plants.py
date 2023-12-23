0b56272e14bd69b475a02cb3753d1ff823bd7ea4
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2023-12-23 23:26:16 +0300

    Added solution for Watering Plants

---
+++ Python-Track/watering-plants.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+
+        steps=0
+        c=capacity
+        for idx,val in enumerate(plants):
+            print(steps)
+            if c<val:
+                steps+=(idx)*2+1
+                c=capacity
+            else:
+                steps+=1
+            c-=val
+        return steps
+
+        
