a93ebada290da991471536e29eddb5957972f958
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-02-23 13:53:47 +0300

    Add solution for Simplify Path

---
+++ progress_sheet/leetcode/simplify-path.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        paths=path.split('/')
+        stack=[]
+        for path in paths:
+            if (not path) or path=='.':
+                continue
+            if path=='..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append('/'+ path)
+        if not stack:
+            return '/'
+        return ''.join(stack)
+            
+
+        
