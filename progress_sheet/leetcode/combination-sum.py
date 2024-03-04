58c90552e099bbb2e603c12565b5b23053644283
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-03-04 20:27:59 +0300

    Add solution for Combination Sum

---
+++ progress_sheet/leetcode/combination-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+
+
+        ans = []
+        def helper(idx, path, sum):
+            if sum == target:
+                ans.append(path.copy())
+                return
+            if sum > target or idx == len(candidates):
+                return
+            
+            path.append(candidates[idx])
+            sum +=candidates[idx]
+
+            helper(idx, path, sum)
+            sum -= path.pop()
+            helper(idx + 1, path, sum)
+
+        helper(0, [], 0)
+        return ans
+
+        
