75d80ccbc20664eff3af1970a3fc4df86d1e2b78
Brookt1 <99480257+Brookt1@users.noreply.github.com>
2024-03-06 16:37:07 +0300

    Add solution for Search a 2D Matrix

---
+++ progress_sheet/leetcode/search-a-2d-matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left, right = 0, n*m -1
+        while left <= right:
+            mid = left + (right - left)//2
+            if matrix[mid // n][mid % n] > target:   
+                right = mid - 1
+            elif matrix[mid // n][mid % n] < target:
+                left = mid + 1
+            else:
+                return True
+        return False
